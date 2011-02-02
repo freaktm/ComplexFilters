7075ae36e3eef22a3a1aa73bc433d06023ca7567
CompSci temp account 2 <cs002@nui.cs.waikato.ac.nz>
2011-02-02 15:01:42 +1300

    converting to float32

--- behav.vhdl
+++ behav.vhdl
@@ -22,13 +22,13 @@ entity filters is
     etrans_im : out float32;
     otrans_re : out float32;
     otrans_im : out float32;
-    uf        : in  real;
-    vf        : in  real;
-    wf        : in  real;
-    theta     : in  real;
-    oeval     : in  real;
-    stval     : in  real;
-    mtspeed   : in  real
+    uf        : in  float32;
+    vf        : in  float32;
+    wf        : in  float32;
+    theta     : in  float32;
+    oeval     : in  float32;
+    stval     : in  float32;
+    mtspeed   : in  float32
     );
 
 end filters;
@@ -55,42 +55,41 @@ architecture Behavioral of filters is
   constant kc2       : integer := 41;
   constant ks1       : integer := 43;
   constant ks2       : integer := kc2+ks1-kc1;
+  constant g         : real    := 0.25;
 
-  constant sfixed_exp_size : integer := 21;
-  constant sfixed_dec_size : integer := 10;
 
-  signal speed               : real := 0.0;
-  signal u0                  : real := 0.0;
-  signal scale               : real := 0.0;
-  signal sigys               : float32;
+  signal sigys          : float32;
+  signal sigys_pi       : float32;
+  signal sigys_pi_vdash : float32;
+
+  signal speed               : float32;
+  signal u0                  : float32;
+  signal scale               : float32;
   signal udash               : real := 0.0;
   signal shilb_im            : real := 0.0;
   signal hz                  : real := 0.0;
   signal stratio             : real := 0.0;
   signal udash_pi            : real := 0.0;
-  signal ang                 : real := 0.0;
+  signal ang                 : float32;
   signal ang_S, ang_C, grad  : real := 0.0;
   signal S                   : real := 0.0;
   signal scale_S, scale_C    : real := 0.0;
   signal xc1, xc2, xs1, xs2  : real := 0.0;
   signal r, t                : real := 0.0;
   signal p, q                : real := 0.0;
-  signal g                   : real := 0.25;
   signal r1, p1              : real := 0.0;
   signal vdash               : real := 0.0;
   signal w                   : real := 0.0;
-  signal sigys_pi            : sfixed(sfixed_exp_size downto sfixed_dec_size);
-  signal sigys_pi_vdash      : sfixed(sfixed_exp_size downto sfixed_dec_size);
   signal temp1, temp2, temp3 : real := 0.0;
-  signal temp4               : sfixed(sfixed_exp_size downto sfixed_dec_size);
+-- signal temp4 : sfixed(sfixed_exp_size downto sfixed_dec_size);
   signal temp5_im            : real := 0.0;
   signal tphase_S, tphase_C  : real := 0.0;
   signal etsust_re           : real := 0.0;
   signal etsust_im           : real := 0.0;
   signal thilb_im            : real := 0.0;
-  signal tempy, tempx        : sfixed(sfixed_exp_size downto sfixed_dec_size);
-  signal espsust             : sfixed(sfixed_exp_size downto sfixed_dec_size);
-  signal esptrans            : sfixed(sfixed_exp_size downto sfixed_dec_size);
+-- signal tempy, tempx : sfixed(sfixed_exp_size downto sfixed_dec_size);
+-- signal espsust : sfixed(sfixed_exp_size downto sfixed_dec_size);
+-- signal esptrans : sfixed(sfixed_exp_size downto sfixed_dec_size);
   signal temp6_re            : real := 0.0;
   signal temp6_im            : real := 0.0;
   signal ettrans_im          : real := 0.0;
@@ -118,13 +117,34 @@ architecture Behavioral of filters is
   signal otrans_re_int : float32;
   signal otrans_im_int : float32;
 
-  signal uf_int      : real := 0.0;
-  signal vf_int      : real := 0.0;
-  signal wf_int      : real := 0.0;
-  signal theta_int   : real := 0.0;
-  signal oeval_int   : real := 0.0;
-  signal stval_int   : real := 0.0;
-  signal mtspeed_int : real := 0.0;
+  signal uf_int      : float32;
+  signal vf_int      : float32;
+  signal wf_int      : float32;
+  signal theta_int   : float32;
+  signal oeval_int   : float32;
+  signal stval_int   : float32;
+  signal mtspeed_int : float32;
+
+
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  -- float32 signals converted to real for simulation only  -------------------
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  --synthesis translate_off
+  signal sigys_real : real := 0.0;
+  signal u0_real    : real := 0.0;
+  signal scale_real : real := 0.0;
+  signal speed_real : real := 0.0;
+  signal ang_real   : real := 0.0;
+  --synthesis translate_on
+
+
+
+
+
+
 
 begin  -- Behavioral
 
@@ -156,134 +176,151 @@ begin  -- Behavioral
   end process p_output_registers;
 
 
-  u0    <= real(peakhz)/mtspeed_int;
-  scale <= mtspeed_int*(3.0/real(peakhz));
-  sigys <= to_float((1.4*real(aspect))/u0);
-  speed <= (real(kratio)*u0)/1.0;
+  u0    <= to_float(peakhz)/mtspeed_int;
+  sigys <= (1.4*to_float(aspect))/u0;
+  scale <= mtspeed_int*(3.0/to_float(peakhz));
+  speed <= 1.0/(to_float(kratio)*u0);
+  ang   <= theta * to_float(con);
 
-  ang   <= theta * con;
-  grad  <= tan(ang * 90.0*real(con));
-  ang_S <= sin(ang);
-  ang_C <= cos(ang);
-
-  udash <= (vf_int*ang_S)+(uf_int*ang_C);
-
-
-  p_shilb : process (ang, uf_int, vf_int)
-  begin  -- process p_shilb
-    if (ang = 0.0) then
-      if (uf_int <= 0.0) then
-        shilb_im <= 1.0;
-      else
-        shilb_im <= -1.0;
-      end if;
-    else
-      if (vf_int <= grad) then
-        shilb_im <= 1.0;
-      else
-        shilb_im <= -1.0;
-      end if;
-    end if;
-  end process p_shilb;
-
-  p_hz_check : process (hz)
-  begin  -- process p_hz_check
-    hz   <= speed * udash;
-    if (hz = 0.0) then
-      hz <= 0.001;
-    end if;
-  end process p_hz_check;
-
-  stratio <= (abs(hz)*real(kratio))/1.0;
-
-  udash_pi <= (MATH_PI*MATH_PI)*(udash**2);
-
-  S       <= scale*(8.23/60.0)*(2.0*MATH_PI);
-  scale_S <= sin(S);
-  scale_C <= cos(S);
-
-  xc1 <= scale*(2.22/60.0);
-  xc2 <= scale*(4.97/60.0);
-  xs1 <= scale*(15.36/60.0);
-  xs2 <= scale*(17.41/60.0);
-
-  r <= real(kc2) * (exp(-1.0 * ((xs2**2)*udash_pi)));
-  t <= real(ks2) * (exp(-1.0 * ((xs2**2)*udash_pi)));
-  p <= real(kc1) * (exp(-1.0 * ((xc1**2)*udash_pi)));
-  q <= real(ks1) * (exp(-1.0 * ((xs1**2)*udash_pi)));
-
-  p1 <= p + q;
-  r1 <= r - t;
-
-  vdash          <= ((-1.0 * uf_int)*ang_S)+(vf_int*ang_C);
-  w              <= wInterval * wf_int;
-  sigys_pi       <= to_sfixed(sigys, sfixed_exp_size, sfixed_dec_size) * to_sfixed(MATH_PI, sfixed_exp_size, sfixed_dec_size);
-  sigys_pi_vdash <= sigys_pi * to_sfixed(vdash, sfixed_exp_size, sfixed_dec_size);
-  temp1          <= (scale_C*((p1**2)+(-1.0 * (2.0 * (scale_C * p1)))))**2;
-  temp2          <= temp1 + (r1 * (scale_C*(1.0-2.0*g)))**2;
-  temp4          <= sigys_pi * to_sfixed(exp(-1.0 * (to_real(sigys_pi_vdash)**2)), sfixed_exp_size, sfixed_dec_size);
-
-  tempy <= to_sfixed(SQRT(to_real(temp4)), sfixed_exp_size, sfixed_dec_size);
-  tempx <= to_sfixed(SQRT(temp2), sfixed_exp_size, sfixed_dec_size);
-
-  tphase_C <= cos(w * (2.0*MATH_PI*tphase));
-  tphase_S <= sin(w * (2.0*MATH_PI*tphase));
-
-  temp3    <= tphase_C * (exp(-0.5*((tsd**2)*(w**2))));
-  temp5_im <= tphase_C * (exp(-0.5*((tsd**2)*(w**2))));
-
-  etsust_re <= temp3;
-  etsust_im <= temp5_im;
-
-  espsust  <= tempy * tempx;
-  esptrans <= espsust * to_sfixed(stratio, sfixed_exp_size, sfixed_dec_size);
-
-  temp6_im   <= w*etsust_im;
-  temp6_re   <= w*etsust_re;
-  ettrans_re <= kratio * temp6_im;
-  ettrans_im <= kratio * temp6_re;
-
-  emain_re <= -1.0 * (ettrans_re * esptrans);
-  emain_im <= -1.0 * (ettrans_im * esptrans);
-
-  thilb_im <= SIGN(wf_int);
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  -- float32 signals converted to real for simulation only  -------------------
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  -----------------------------------------------------------------------------
+  --synthesis translate_off
+  u0_real    <= To_real(u0);
+  sigys_real <= To_real(sigys);
+  scale_real <= To_real(scale);
+  speed_real <= To_real(speed);
+  ang_real   <= To_real(ang);
+  --synthesis translate_on
 
 
 
-  p_thilb : process (thilb_im)
-  begin  -- process p_thilb
-    if thilb_im = 0.0 then
-      thilb_ettrans_re <= ettrans_re;
-      thilb_ettrans_im <= ettrans_im;
-    else
-      thilb_ettrans_im <= thilb_im * ettrans_re;
-      thilb_ettrans_re <= thilb_im * ettrans_im;
-    end if;
-  end process p_thilb;
+
+-- grad <= tan(ang * 90.0*real(con));
+-- ang_S <= sin(ang);
+-- ang_C <= cos(ang);
+
+-- udash <= (vf_int*ang_S)+(uf_int*ang_C);
 
 
-  esust_re_int <= to_float(espsust * etsust_re);
-  esust_im_int <= to_float(espsust * etsust_im);
+-- p_shilb : process (ang, uf_int, vf_int)
+-- begin                                -- process p_shilb
+--     if (ang = 0.0) then
+--       if (uf_int <= 0.0) then
+--         shilb_im <= 1.0;
+--       else
+--         shilb_im <= -1.0;
+--       end if;
+--     else
+--       if (vf_int <= grad) then
+--         shilb_im <= 1.0;
+--       else
+--         shilb_im <= -1.0;
+--       end if;
+--     end if;
+--   end process p_shilb;
 
-  osust_re_int <= esust_im_int * to_float(shilb_im);
-  osust_im_int <= esust_re_int * to_float(shilb_im);
+-- p_hz_check : process (hz)
+-- begin                                -- process p_hz_check
+--     hz   <= speed * udash;
+--     if (hz = 0.0) then
+--       hz <= 0.001;
+--     end if;
+--   end process p_hz_check;
 
-  shilb_esptrans_im <= esptrans * shilb_im;
-  shilb_esptrans_re <= shilb_im * shilb_esptrans_im;
-  ehilb_re          <= thilb_ettrans_im * shilb_esptrans_im;
-  ehilb_im          <= shilb_esptrans_im * thilb_ettrans_re;
+-- stratio <= (abs(hz)*real(kratio))/1.0;
 
-  etrans_re_int <= to_float(-1.0 * ((-1.0 * emain_re) + ehilb_re));
-  etrans_im_int <= to_float(-1.0 * ((-1.0 * emain_im) + ehilb_im));
+-- udash_pi <= (MATH_PI*MATH_PI)*(udash**2);
 
-  omain_im <= -1.0 * (shilb_esptrans_im * ettrans_re);
-  omain_re <= -1.0 * (shilb_esptrans_im * ettrans_im);
+-- S <= scale*(8.23/60.0)*(2.0*MATH_PI);
+-- scale_S <= sin(S);
+-- scale_C <= cos(S);
 
-  ohilb_re <= shilb_esptrans_re * thilb_ettrans_re;
-  ohilb_im <= thilb_ettrans_im * shilb_esptrans_re;
+-- xc1 <= scale*(2.22/60.0);
+-- xc2 <= scale*(4.97/60.0);
+-- xs1 <= scale*(15.36/60.0);
+-- xs2 <= scale*(17.41/60.0);
 
-  otrans_re_int <= to_float(-1.0 * ((ohilb_im * omain_im) + (ohilb_re * omain_re)));
-  otrans_im_int <= to_float(-1.0 * ((ohilb_im * omain_re) + (ohilb_re * omain_im)));
+-- r <= real(kc2) * (exp(-1.0 * ((xs2**2)*udash_pi)));
+-- t <= real(ks2) * (exp(-1.0 * ((xs2**2)*udash_pi)));
+-- p <= real(kc1) * (exp(-1.0 * ((xc1**2)*udash_pi)));
+-- q <= real(ks1) * (exp(-1.0 * ((xs1**2)*udash_pi)));
+
+-- p1 <= p + q;
+-- r1 <= r - t;
+
+-- vdash <= ((-1.0 * uf_int)*ang_S)+(vf_int*ang_C);
+-- w <= wInterval * wf_int;
+-- sigys_pi <= to_sfixed(sigys, sfixed_exp_size, sfixed_dec_size) * to_sfixed(MATH_PI, sfixed_exp_size, sfixed_dec_size);
+-- sigys_pi_vdash <= sigys_pi * to_sfixed(vdash, sfixed_exp_size, sfixed_dec_size);
+-- temp1 <= (scale_C*((p1**2)+(-1.0 * (2.0 * (scale_C * p1)))))**2;
+-- temp2 <= temp1 + (r1 * (scale_C*(1.0-2.0*g)))**2;
+-- temp4 <= sigys_pi * to_sfixed(exp(-1.0 * (to_real(sigys_pi_vdash)**2)), sfixed_exp_size, sfixed_dec_size);
+
+-- tempy <= to_sfixed(SQRT(to_real(temp4)), sfixed_exp_size, sfixed_dec_size);
+-- tempx <= to_sfixed(SQRT(temp2), sfixed_exp_size, sfixed_dec_size);
+
+-- tphase_C <= cos(w * (2.0*MATH_PI*tphase));
+-- tphase_S <= sin(w * (2.0*MATH_PI*tphase));
+
+-- temp3 <= tphase_C * (exp(-0.5*((tsd**2)*(w**2))));
+-- temp5_im <= tphase_C * (exp(-0.5*((tsd**2)*(w**2))));
+
+-- etsust_re <= temp3;
+-- etsust_im <= temp5_im;
+
+-- espsust <= tempy * tempx;
+-- esptrans <= espsust * to_sfixed(stratio, sfixed_exp_size, sfixed_dec_size);
+
+-- temp6_im <= w*etsust_im;
+-- temp6_re <= w*etsust_re;
+-- ettrans_re <= kratio * temp6_im;
+-- ettrans_im <= kratio * temp6_re;
+
+-- emain_re <= -1.0 * (ettrans_re * esptrans);
+-- emain_im <= -1.0 * (ettrans_im * esptrans);
+
+-- thilb_im <= SIGN(wf_int);
+
+
+
+-- p_thilb : process (thilb_im)
+-- begin                                -- process p_thilb
+--     if thilb_im = 0.0 then
+--       thilb_ettrans_re <= ettrans_re;
+--       thilb_ettrans_im <= ettrans_im;
+--     else
+--       thilb_ettrans_im <= thilb_im * ettrans_re;
+--       thilb_ettrans_re <= thilb_im * ettrans_im;
+--     end if;
+--   end process p_thilb;
+
+
+-- esust_re_int <= to_float(espsust * etsust_re);
+-- esust_im_int <= to_float(espsust * etsust_im);
+
+-- osust_re_int <= esust_im_int * to_float(shilb_im);
+-- osust_im_int <= esust_re_int * to_float(shilb_im);
+
+-- shilb_esptrans_im <= esptrans * shilb_im;
+-- shilb_esptrans_re <= shilb_im * shilb_esptrans_im;
+-- ehilb_re <= thilb_ettrans_im * shilb_esptrans_im;
+-- ehilb_im <= shilb_esptrans_im * thilb_ettrans_re;
+
+-- etrans_re_int <= to_float(-1.0 * ((-1.0 * emain_re) + ehilb_re));
+-- etrans_im_int <= to_float(-1.0 * ((-1.0 * emain_im) + ehilb_im));
+
+-- omain_im <= -1.0 * (shilb_esptrans_im * ettrans_re);
+-- omain_re <= -1.0 * (shilb_esptrans_im * ettrans_im);
+
+-- ohilb_re <= shilb_esptrans_re * thilb_ettrans_re;
+-- ohilb_im <= thilb_ettrans_im * shilb_esptrans_re;
+
+-- otrans_re_int <= to_float(-1.0 * ((ohilb_im * omain_im) + (ohilb_re * omain_re)));
+-- otrans_im_int <= to_float(-1.0 * ((ohilb_im * omain_re) + (ohilb_re * omain_im)));
 
 
 end Behavioral;
--- filters_tb.vhdl
+++ filters_tb.vhdl
@@ -1,32 +1,12 @@
 -------------------------------------------------------------------------------
--- Title      : Testbench for design "filters"
--- Project    : 
--------------------------------------------------------------------------------
--- File       : filters_tb.vhdl
--- Author     : Aaron Storey  <freaktm@freaktm>
--- Company    : 
--- Created    : 2011-01-29
--- Last update: 2011/02/01
--- Platform   : 
--- Standard   : VHDL'87
--------------------------------------------------------------------------------
--- Description: 
--------------------------------------------------------------------------------
--- Copyright (c) 2011 
--------------------------------------------------------------------------------
--- Revisions  :
--- Date        Version  Author  Description
--- 2011-01-29  1.0      freaktm Created
--------------------------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
-
 library floatfixlib;
 use floatfixlib.float_pkg.all;
+use floatfixlib.fixed_pkg.all;
 
 -------------------------------------------------------------------------------
 
@@ -49,171 +29,83 @@ architecture tb of filters_tb is
       etrans_im : out float32;
       otrans_re : out float32;
       otrans_im : out float32;
-      uf        : in  real;
-      vf        : in  real;
-      wf        : in  real;
-      theta     : in  real;
-      oeval     : in  real;
-      stval     : in  real;
-      mtspeed   : in  real
-      );
+      uf        : in  float32;
+      vf        : in  float32;
+      wf        : in  float32;
+      theta     : in  float32;
+      oeval     : in  float32;
+      stval     : in  float32;
+      mtspeed   : in  float32);
   end component;
 
-  -- component ports
-  signal esust_im  : float32;
-  signal esust_re  : float32;
-  signal osust_re  : float32;
-  signal osust_im  : float32;
-  signal etrans_re : float32;
-  signal etrans_im : float32;
-  signal otrans_re : float32;
-  signal otrans_im : float32;
-  signal uf        : real := -20.0;
-  signal vf        : real := -20.0;
-  signal wf        : real := -4.0;
-  signal theta     : real := 0.0;
-  signal oeval     : real := 0.0;
-  signal stval     : real := 0.0;
-  signal mtspeed   : real := 1.0;
-
-  -- clock
-  signal Clk : std_logic := '1';
-
-  constant X_SIZE : integer := 128;
-  constant Y_SIZE : integer := 128;
-
-  type frame_t is array (X_SIZE-1 downto 0, Y_SIZE-1 downto 0) of real;
-
-  signal frame_esust_re  : frame_t;
-  signal frame_esust_im  : frame_t;
-  signal frame_osust_re  : frame_t;
-  signal frame_osust_im  : frame_t;
-  signal frame_etrans_re : frame_t;
-  signal frame_etrans_im : frame_t;
-  signal frame_otrans_re : frame_t;
-  signal frame_otrans_im : frame_t;
-
-  type filter_set_t is
-  record
-    esust_re  : frame_t;
-    esust_im  : frame_t;
-    osust_re  : frame_t;
-    osust_im  : frame_t;
-    etrans_re : frame_t;
-    etrans_im : frame_t;
-    otrans_re : frame_t;
-    otrans_im : frame_t;
-  end record;
+  signal clk_i       : std_logic := '1';
+  signal esust_im_i  : float32;
+  signal esust_re_i  : float32;
+  signal osust_re_i  : float32;
+  signal osust_im_i  : float32;
+  signal etrans_re_i : float32;
+  signal etrans_im_i : float32;
+  signal otrans_re_i : float32;
+  signal otrans_im_i : float32;
+  signal uf_i        : float32;
+  signal vf_i        : float32;
+  signal wf_i        : float32;
+  signal theta_i     : float32;
+  signal oeval_i     : float32;
+  signal stval_i     : float32;
+  signal mtspeed_i   : float32;
 
 
-  impure function load_filter_set_from_file(filename : in string)
-    return filter_set_t is
-
-    type real_file is file of real;
-    file my_file : real_file;
-
-    variable filter_set_0 : filter_set_t;
-    
-  begin
-    file_open(my_file, filename, read_mode);
-
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.esust_im(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.esust_im(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.osust_re(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.osust_im(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.etrans_re(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.etrans_im(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.otrans_re(i, j));
-      end loop;
-    end loop;
-    for i in 0 to X_SIZE-1 loop
-      for j in 0 to Y_SIZE-1 loop
-        read(my_file, filter_set_0.otrans_im(i, j));
-      end loop;
-    end loop;
-
-
-    file_close(my_file);
-    return filter_set_0;
-    
-  end function;
-
-
-  signal filter_set_0 : filter_set_t := load_filter_set_from_file("nFilter1.1.dat");
-
-
+  --synthesis translate_off
+  signal mtspeed_i_real : real := 0.0;
+  signal sigys_real     : real := 0.0;
+  signal theta_i_real   : real := 0.0;
+  --synthesis translate_on
 begin  -- tb
 
-  -- component instantiation
+
+
   DUT : filters
     port map (
-      clk => clk,
-      esust_im  => esust_im,
-      esust_re  => esust_re,
-      osust_re  => osust_re,
-      osust_im  => osust_im,
-      etrans_re => etrans_re,
-      etrans_im => etrans_im,
-      otrans_re => otrans_re,
-      otrans_im => otrans_im,
-      uf        => uf,
-      vf        => vf,
-      wf        => wf,
-      theta     => theta,
-      oeval     => oeval,
-      stval     => stval,
-      mtspeed   => mtspeed
-      );
+      clk       => clk_i,
+      esust_im  => esust_im_i,
+      esust_re  => esust_re_i,
+      osust_re  => osust_re_i,
+      osust_im  => osust_im_i,
+      etrans_re => etrans_re_i,
+      etrans_im => etrans_im_i,
+      otrans_re => otrans_re_i,
+      otrans_im => otrans_im_i,
+      uf        => uf_i,
+      vf        => vf_i,
+      wf        => wf_i,
+      theta     => theta_i,
+      oeval     => oeval_i,
+      stval     => stval_i,
+      mtspeed   => mtspeed_i);
+
 
   -- clock generation
-  Clk <= not Clk after 10 ns;
-
-  -- waveform generation
-  WaveGen_Proc : process
-  begin
-    -- insert signal assignments here
-
-    wait for 100 ns;
-    wait until Clk = '1';
-    report "simulation finished" severity warning;
-    wait;
-  end process WaveGen_Proc;
+  clk_i <= not clk_i after 10 ns;
 
 
-  
+  -- set mtspeed to 1
+  mtspeed_i <= to_float(1.0);
+
+  -- set theta to 6
+  theta_i <= to_float(6.0);
+
+
+  -- synthesis translate_off 
+  mtspeed_i_real <= To_real(mtspeed_i);
+  theta_i_real   <= To_real(theta_i);
+  -- synthesis translate_on
+
+
+
+
+
+
 end tb;
-
--------------------------------------------------------------------------------
-
-configuration filters_tb_tb_cfg of filters_tb is
-  for tb
-  end for;
-end filters_tb_tb_cfg;
 
 -------------------------------------------------------------------------------
