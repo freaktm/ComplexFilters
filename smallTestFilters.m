494e79724c3b6ef27068899905887845573e0f57
freaktm <FreaKtm@FreaKtm-PC.(none)>
2012-02-27 13:28:02 +1300

    removed loops and other variables, added in constants for the function and changed function name

--- smallTestFilters.m
+++ smallTestFilters.m
@@ -1,4 +1,4 @@
-function f = testFilters(mtspeed)
+function f = smallTestFilters(mtspeed)
 % Version which uses FFT function to create full set of filters
 
 % Note filters are stored in order (0 30 330 60 300 90 270 120 240 150 210
@@ -6,7 +6,7 @@ function f = testFilters(mtspeed)
 % Writes to file.
 
 % John Perrone May 2010
-str1 = 'nFilterML';
+str1 = 'nFilterSmall';
 str2 = '.txt';
 str3 = num2str(mtspeed); 
 
@@ -33,40 +33,28 @@ wvals = [-thalf:thalf-1];
 x1 = [-umax:deltu:umax];
 y1 = [-umax:deltu:umax];
 
-nk = 1;
+angNum = 1; 1 to 12
+wfNum = 1; 1 to nframes
+vfNum = 1; 1 to ysize
+ufNum = 1; 1 to xsize
 
 
-for ntheta = 1:length(theta)
-    [mtspeed theta(ntheta)] 
-    wimang = theta(ntheta);
+    wimang = theta(angNum);
     ang = wimang * con; 
     str4 = num2str(nk);
     str5 = strcat(str1, str3, '.', str4, str2); 	
     dataFile = fopen(str5, 'wd');
-	
-    for nn = 1:8
-        wf = wvals(nn);
-        for ny = 1:ysize
-            vf = y1(ny);
-            for nx = 1:xsize
-                uf = x1(nx);              
-                [esust, osust, etrans, otrans] = createComplexFiltervals(uf, vf, wf, ang, 0, 0, mtspeed, 40);				
-				inputstring = strcat(num2str(wimang), ',', num2str(wf), ',', num2str(vf), ',', num2str(uf), ',', num2str(uf));
-				fwrite(dataFile, inputstring);				
-                fwrite(dataFile, esust);				
-                fwrite(dataFile, osust);				
-                fwrite(dataFile, etrans);				
-                fwrite(dataFile, otrans);
-            end
-        end
-        
-    end
-    fclose(fid1);
-    
-    nk = nk + 1;
-end
+	wf = wvals(wfNum);
+	vf = y1(vfNum);
+	uf = x1(ufNum);
+	[esust, osust, etrans, otrans] = createComplexFiltervals(uf, vf, wf, ang, 0, 0, mtspeed, 40);	
+	inputstring = strcat(num2str(wimang), ',', num2str(wf), ',', num2str(vf), ',', num2str(uf), ',', num2str(uf));
+	fwrite(dataFile, inputstring);
+	fwrite(dataFile, esust);
+	fwrite(dataFile, osust);
+	fwrite(dataFile, etrans);
+	fwrite(dataFile, otrans);        
+    fclose(fid1); 
 
-
-disp('Filters completed');
-disp(wimang);
+disp('Test completed');
 
